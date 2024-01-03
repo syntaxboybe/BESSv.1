f9e30ce26e729eb998e71a7b3bdbad533b08110b
Syntaxboybe <beboymegalbioeyana@gmail.com>
2024-01-03 22:39:39 +0800

    🔄docs: Update background color in main/index.php

--- main/index.php
+++ main/index.php
@@ -39,7 +39,8 @@
     margin: 0 20px;
     border-radius: 10px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    background-color: #333;
+    transition: 0.3s;
+    background-color: #f5f5f5;
     
   }
 
