5fab0c203bc7fc5b6c7f30beb84376937f8434ad
Syntaxboybe <beboymegalbioeyana@gmail.com>
2024-01-10 11:36:42 +0800

    🆕docs: Update clearance form content

--- pages/clearance/clearance_form.php
+++ pages/clearance/clearance_form.php
@@ -113,7 +113,7 @@
                     <div class="col-xs-12 col-md-12">
                         <p class="text-center" style="font-size: 20px; font-size:bold;">OFFICE OF THE BARANGAY CAPTAIN<br><b style="font-size: 28px;">BARANGAY CLEARANCE</b></p>
                         <p style="font-size: 18px;">TO WHOM IT MAY CONCERN:</p>
-                        <p style="text-indent:40px;text-align: justify;">This is to certify that the person whose photo, signature and right thumb mark appear herein, is a resident of Barangay Poblacion, Naaawan, Misamis Oriental and that the person had requested a criminal check from this office following is/are our findings.</p>
+                        <p style="text-indent:40px;text-align: justify;">This is to certify that the person whose photo appear herein, is a resident of Barangay Poblacion, Naaawan, Misamis Oriental and that the person had requested a clearance.</p>
 
                         <?php
                             $qry=mysqli_query($con,"SELECT * from tblresident r left join tblclearance c on c.residentid = r.id where residentid = '".$_GET['resident']."' and clearanceNo = '".$_GET['clearance']."' ");
@@ -144,41 +144,31 @@
                             }
                         ?>
                     </div>  
-                    <div class="col-md-5 col-xs-4" style="float:right;margin-top: -160px;">
-                        <div style="height:100px; width:140px; border: 1px solid black;">
-                            <center><span style="text-align: center; line-height: 160px;">Right Thumb Mark</span></center>
-                        </div><br><br>
-                        <p>Tax Payer's Signature</p>
-                    </div>
-                </div>
+                   
                 <div class="col-xs-offset-6 col-xs-5 col-md-offset-6 col-md-4" ><br><br><br>
                     <?php
                     $qry = mysqli_query($con,"SELECT * from tblofficial");
                     while($row=mysqli_fetch_array($qry)){
                         if($row['sPosition'] == "Captain"){
                             echo '
-                            <p>
-                            <b style="font-size:18px;">'.strtoupper($row['completeName']).'<br>
-                            <span style=" text-align: center;">Punong Barangay</span></b>
+                            <p <p style="text-align: right;">
+                            <b style="font-size:15px;">'.strtoupper($row['completeName']).'
+                            <span style="float: right;">Punong Barangay</span></b>
                             </p>
+                            <div>
+                            <img src="../../img/signature.png" style="position: absolute; top: 0; right: 0; width: 500px; height: 100px;">
+                            </div>
+                            </div>
                             ';
                         }
                     }
                     ?>
                 </div>
                 <div class="col-xs-8 col-md-4">
-                    <?php
-                    $qry = mysqli_query($con,"SELECT * from tblofficial");
-                    while($row=mysqli_fetch_array($qry)){
-                        if($row['sPosition'] == "Captain"){
-                            echo '
-                            <p>
-                            <b style="font-size:18px;">'.strtoupper($row['completeName']).'<br>
-                            <span style=" text-align: center;">OFFICER OF THE DAY</span></b>
-                            </p>
-                            ';
-                        }
-                    }
+
+                            <p style="text-align: left;">  Not valid without barangay official seal. 
+                            <b style="font-size:15px;"> 
+                            <span style="float: left;"
                     ?>
 
                
