e6253cb50c77c6c6768561a30b85d126d3797f61
Syntaxboybe <beboymegalbioeyana@gmail.com>
2024-01-30 14:29:28 +0800

    Refactor code to improve performance and readability

--- db/db_barangay(empty).sql
+++ db/db_barangay(empty).sql
@@ -39,8 +39,7 @@ CREATE TABLE IF NOT EXISTS `tblactivity` (
 --
 
 INSERT INTO `tblactivity` (`id`, `dateofactivity`, `activity`, `description`) VALUES
-(10, '2017-01-03', 'activity', 'Description'),
-(11, '2017-01-28', 'teets', 'sdfsdfsdfsdf');
+
 
 -- --------------------------------------------------------
 
@@ -60,21 +59,7 @@ CREATE TABLE IF NOT EXISTS `tblactivityphoto` (
 --
 
 INSERT INTO `tblactivityphoto` (`id`, `activityid`, `filename`) VALUES
-(18, 7, '1485255503893ChibiMaker.jpg'),
-(19, 7, '1485255504014dental.jpg'),
-(20, 7, '1485255504108images.jpg'),
-(21, 8, '1485255608251dfxfxfxdfxfxfxdf.png'),
-(22, 8, '1485255608315easy-nail-art-designs-for-beginners-youtube.jpg'),
-(23, 8, '1485255608404Easy-Winter-Nail-Art-Tutorials-2013-2014-For-Beginners-Learners-10.jpg'),
-(24, 8, '1485255608513motherboard.png'),
-(25, 9, '148525575293111041019_1012143402147589_9043399646875097729_n.jpg'),
-(26, 9, '1485255753089bg.PNG'),
-(32, 10, '148526764905211041019_1012143402147589_9043399646875097729_n.jpg'),
-(33, 10, '1485267649364bg.PNG'),
-(34, 10, '1485267649563motherboard.png'),
-(35, 10, '14855301764078196186971_2237f161bd_b.jpg'),
-(36, 10, '1485530481111bicycle-1280x720.jpg'),
-(38, 11, '1485530620716user2.jpg');
+
 
 -- --------------------------------------------------------
 
@@ -107,7 +92,7 @@ CREATE TABLE IF NOT EXISTS `tblblotter` (
 --
 
 INSERT INTO `tblblotter` (`id`, `yearRecorded`, `dateRecorded`, `complainant`, `cage`, `caddress`, `ccontact`, `personToComplain`, `page`, `paddress`, `pcontact`, `complaint`, `actionTaken`, `sStatus`, `locationOfIncidence`, `recordedby`) VALUES
-(3, '2016', '2016-10-15', 'sda, as das', 2132, 'asda', 213, '11', 3213, 'dasda', 2123, '213asd', '1st Option', 'Solved', 'asdsa', 'admin');
+
 
 -- --------------------------------------------------------
 
@@ -134,9 +119,7 @@ CREATE TABLE IF NOT EXISTS `tblclearance` (
 --
 
 INSERT INTO `tblclearance` (`id`, `clearanceNo`, `residentid`, `findings`, `purpose`, `orNo`, `samount`, `dateRecorded`, `recordedBy`, `status`) VALUES
-(8, 0, 11, '', 'asd', 0, 0, '2017-01-20', 'User1', 'New'),
-(9, 1234, 15, 'asdada', 'local employment', 12, 3434, '2017-01-22', 'admin', 'Approved'),
-(10, 123, 11, 'qwe', 'qwe', 213, 2123, '2017-01-24', 'admin', 'Approved');
+
 
 -- --------------------------------------------------------
 
@@ -158,7 +141,7 @@ CREATE TABLE IF NOT EXISTS `tblhousehold` (
 --
 
 INSERT INTO `tblhousehold` (`id`, `householdno`, `zone`, `totalhouseholdmembers`, `headoffamily`) VALUES
-(3, 2, '2', 0, '12');
+
 
 -- --------------------------------------------------------
 
@@ -179,35 +162,7 @@ CREATE TABLE IF NOT EXISTS `tbllogs` (
 --
 
 INSERT INTO `tbllogs` (`id`, `user`, `logdate`, `action`) VALUES
-(2, 'asd', '2017-01-04 00:00:00', 'Added Resident namedjayjay, asd asd'),
-(3, 'asd', '2017-01-04 19:13:40', 'Update Resident named Sample1, User1 Brgy1'),
-(4, 'sad', '2017-01-05 13:22:10', 'Update Official named eliezer a. vacalares, jr.'),
-(7, 'sad', '2017-01-05 13:54:40', 'Update Household Number 1'),
-(8, 'sad', '2017-01-05 14:00:08', 'Update Blotter Request sda, as das'),
-(9, 'sad', '2017-01-05 14:15:39', 'Update Clearance with clearance number of 123131'),
-(10, 'sad', '2017-01-05 14:25:03', 'Update Permit with business name of asda'),
-(11, 'sad', '2017-01-05 14:25:25', 'Update Resident named Sample1, User1 Brgy1'),
-(12, 'Administrator', '2017-01-24 16:32:40', 'Added Permit with business name of hahaha'),
-(13, 'Administrator', '2017-01-24 16:35:41', 'Added Clearance with clearance number of 123'),
-(14, 'Administrator', '2017-01-24 18:43:35', 'Added Activity sad'),
-(15, 'Administrator', '2017-01-24 18:45:49', 'Added Activity qwe'),
-(16, 'Administrator', '2017-01-24 18:46:20', 'Added Activity ss'),
-(17, 'Administrator', '2017-01-24 18:47:39', 'Added Activity e'),
-(18, 'Administrator', '2017-01-24 18:55:20', 'Added Activity activity'),
-(19, 'Administrator', '2017-01-24 18:58:23', 'Added Activity Activity'),
-(20, 'Administrator', '2017-01-24 19:00:07', 'Added Activity activity'),
-(21, 'Administrator', '2017-01-24 19:02:32', 'Added Activity Activity'),
-(22, 'Administrator', '2017-01-24 19:04:54', 'Added Activity activity'),
-(23, 'Administrator', '2017-01-24 19:08:40', 'Update Activity activity'),
-(24, 'Administrator', '2017-01-27 23:23:40', 'Added Activity teets'),
-(25, 'Administrator', '2017-01-27 23:24:14', 'Update Resident named Sample1, User1 Brgy1'),
-(26, 'Administrator', '2017-01-27 23:25:10', 'Update Resident named sda, as das'),
-(27, 'Administrator', '2017-01-30 10:45:13', 'Added Resident named 2, 2 2'),
-(28, 'Administrator', '2017-01-30 10:45:54', 'Added Resident named 2, 2 2'),
-(29, 'Administrator', '2017-02-06 08:58:23', 'Update Resident named sda, as das'),
-(30, 'Administrator', '2017-02-06 09:00:14', 'Update Resident named sda, as das'),
-(31, 'Administrator', '2017-02-06 09:03:57', 'Added Household Number 2'),
-(32, 'Administrator', '2017-02-06 09:04:25', 'Added Household Number 2');
+
 
 -- --------------------------------------------------------
 
@@ -232,14 +187,7 @@ CREATE TABLE IF NOT EXISTS `tblofficial` (
 --
 
 INSERT INTO `tblofficial` (`id`, `sPosition`, `completeName`, `pcontact`, `paddress`, `termStart`, `termEnd`, `status`) VALUES
-(4, 'Captain', 'eliezer a. vacalares, jr.', '8978768761', 'gdfgdfgd', '2016-10-03', '2016-10-06', 'Ongoing Term'),
-(5, 'Kagawad(Ordinance)', 'ramil d. pakino', '4234', 'sfdsa', '2016-10-31', '2016-11-17', 'Ongoing Term'),
-(6, 'Kagawad(Public Safety)', 'chito t. epal', '234234', '45sdfdf', '2016-10-31', '2016-11-24', 'Ongoing Term'),
-(7, 'Kagawad(Tourism)', 'debie v. pereyra', '67567', 'sdfgf543', '2016-11-13', '2016-12-01', 'Ongoing Term'),
-(8, 'Kagawad(Budget & Finance)', 'milard t. muring', '35454', 'dfgfgxcg', '2016-11-06', '2016-11-30', 'Ongoing Term'),
-(9, 'Kagawad(Agriculture)', 'jaime d. abella', '3453545', 'sfsfds', '2016-11-06', '2016-11-22', 'Ongoing Term'),
-(10, 'Kagawad(Education)', 'eugene j. labo', '4245', 'vxcsaf', '2016-11-06', '2016-11-25', 'Ongoing Term'),
-(11, 'Kagawad(Infrastracture)', 'lerma a. abesamis', '231', 'xcvs', '2016-10-31', '2016-11-28', 'Ongoing Term');
+
 
 -- --------------------------------------------------------
 
@@ -266,13 +214,7 @@ CREATE TABLE IF NOT EXISTS `tblpermit` (
 --
 
 INSERT INTO `tblpermit` (`id`, `residentid`, `businessName`, `businessAddress`, `typeOfBusiness`, `orNo`, `samount`, `dateRecorded`, `recordedBy`, `status`) VALUES
-(2, 11, 'test', 'test', 'Option 2', 213, 2132131, '2016-10-15', '', 'Disapproved'),
-(3, 11, 'asda', 'sdfs', 'Option 1', 43434, 45454, '2016-10-15', 'admin', 'Approved'),
-(4, 11, '23', 'asda', 'Option 1', 342, 434543, '2016-10-15', 'admin', 'Approved'),
-(5, 11, 'Business ', 'Address', 'Option 1', 0, 0, '2016-12-04', 'a', 'New'),
-(6, 11, 'sa', 'sa', 'Option 1', 2, 12, '2017-01-20', 'a', 'Approved'),
-(7, 11, 'sad', 'asd', 'Option 2', 0, 0, '2017-01-20', 'a', 'New'),
-(8, 12, 'hahaha', 'hehe', 'Option 1', 1234, 1234, '2017-01-24', 'admin', 'Approved');
+
 
 -- --------------------------------------------------------
 
@@ -326,12 +268,7 @@ CREATE TABLE IF NOT EXISTS `tblresident` (
 --
 
 INSERT INTO `tblresident` (`id`, `lname`, `fname`, `mname`, `bdate`, `bplace`, `age`, `barangay`, `zone`, `totalhousehold`, `differentlyabledperson`, `relationtohead`, `maritalstatus`, `bloodtype`, `civilstatus`, `occupation`, `monthlyincome`, `householdnum`, `lengthofstay`, `religion`, `nationality`, `gender`, `skills`, `igpitID`, `philhealthNo`, `highestEducationalAttainment`, `houseOwnershipStatus`, `landOwnershipStatus`, `dwellingtype`, `waterUsage`, `lightningFacilities`, `sanitaryToilet`, `formerAddress`, `remarks`, `image`, `username`, `password`) VALUES
-(11, 'Sample1', 'User1', 'Brgy1', '2017-01-01', 'dfsd1', 1, 'asdf', '2', 3, 'asdf', 'asdf', 'saf', 'sadf', 'fsd', 'adfs', 1, 1, 7, 'asd', 'asd', 'Female', 'asda1', 2, 2211, 'Doctorate degree', 'Live with Parents/Re', '1st Option', '1st Option', 'dsad11', '2211', 'dsfs11', 'ddsfd111', 'fdgfd11', '1482037013441_bg.PNG', 'a', 'a'),
-(12, 'sda', 'as', 'das', '2016-01-01', 'adda', 1, '2323', '0', 5, '', '', '', '', '', '', 45, 2, 6, 'ada', 'sda', 'Male', 'sasda', 2, 3, 'Elementary', 'Rent', '2nd Option', '1st Option', 'sadas', '3', 'asdas', 'dsada', 'dsadsa', '1486342814017_Beach-Wallpapers.jpg', 'asd', 'asda'),
-(13, 'a', 'asd', 'das', '2016-12-31', 'asdf', 2, '', '0', 0, '', '', '', '', '', '', 234, 3, 1, 'asd', 'asdf', '', 'saf', 3, 3, 'No schooling completed', 'Own Home', '1st Option', '1st Option', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'default.png', '1', '1'),
-(14, 'sdf', 'das', 'das', '2016-01-01', 'dsf', 2, '', '0', 0, '', '', '', '', '', '', 234, 2, 3, 'asdf', 'asdf', 'Male', 'asf', 2, 2, 'No schooling completed', 'Own Home', '1st Option', '1st Option', 'asf', 'sdf', 'asdf', 'asdf', 'asd', '1482037013441_bg.PNG', 'asdf', 'asdf'),
-(15, 'jay', 'dsf', 'asdf', '2017-01-19', 'sdfa', 2, 'sdaf', '23', 23, 'sdf', 'adf', 'asd', 'asdf', 'adf', 'adsf', 23123, 23, 23, 'asd', 'dsf', 'Male', 'asdf', 23, 23, 'No schooling completed', 'Own Home', 'Owned', '1st Option', 'Faucet', 'Electric', 'Water-sealed', 'asdfa', 'asfa', 'default.png', 'qwe', 'qwe'),
-(16, 'jayjay', 'asd', 'asd', '2017-01-02', 'sad', 23, 'asd', '23', 23, 'ad', 'asd', 'asd', 'as', 'asd', 'asd', 43, 23, 23, 'asd', 'asd', 'Male', 'sad', 23, 23, 'No schooling completed', 'Own Home', 'Owned', '1st Option', 'Faucet', 'Electric', 'Water-sealed', 'asd', 'asd', 'default.png', 'a', 'sa');
+
 
 -- --------------------------------------------------------
 
@@ -352,7 +289,7 @@ CREATE TABLE IF NOT EXISTS `tblstaff` (
 --
 
 INSERT INTO `tblstaff` (`id`, `name`, `username`, `password`) VALUES
-(1, 'sad', 'asd', 'asd');
+
 
 -- --------------------------------------------------------
 
@@ -373,8 +310,7 @@ CREATE TABLE IF NOT EXISTS `tbluser` (
 --
 
 INSERT INTO `tbluser` (`id`, `username`, `password`, `type`) VALUES
-(1, 'admin', 'admin', 'administrator'),
-(2, 'zone', '1234', 'zoneleader');
+
 
 -- --------------------------------------------------------
 
@@ -394,8 +330,8 @@ CREATE TABLE IF NOT EXISTS `tblzone` (
 -- Dumping data for table `tblzone`
 --
 
-INSERT INTO `tblzone` (`id`, `zone`, `username`, `password`) VALUES
-(2, '4', 'a', 'a');
+INSERT INTO `tblcaptain` (`id`, `captain`, `username`, `password`) VALUES
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
