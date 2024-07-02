dd4ea36f503aacfcbf70e2f8837a6914fdd31c19
Syntaxboybe <beboymegalbioeyana@gmail.com>
2024-07-02 12:03:07 +0800

    fix: routes

---
+++ public/about.php
@@ -0,0 +1,95 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Barangay E-service System</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- bootstrap 3.0.2 -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- font Awesome -->
+    <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Ionicons -->
+    <link href="../css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <link href="../js/morris/morris-0.4.3.min.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="../css/AdminLTE.css" rel="stylesheet" type="text/css" />
+
+    <link href="../css/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="../css/select2.css" rel="stylesheet" type="text/css" />
+    <script src="../js/jquery-1.12.3.js" type="text/javascript"></script>
+
+</head>
+<body>
+<nav class="navbar navbar-inverse" style="border-radius:0px;">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php"><img alt="Brand" src="../img/poblacionlogo.png" style="width:50px; margin-top:-15px; "></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="javascript:void(0)" onclick="redirectLogin()">Login</a></li>
+        
+        <script>
+        function redirectLogin() {
+            var currentUrl = window.location.href;
+        
+            if (currentUrl.includes("pages/resident")) {
+                window.location.href = "../pages/resident/login.php";
+            } else if (currentUrl.includes("pages/zone")) {
+                window.location.href = "../pages/zone/login.php";
+            } else {
+                window.location.href = "../login.php";
+            }
+        }
+        </script>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<div class="wrapper row-offcanvas row-offcanvas-left">
+
+<div class="breadcrumb justify-content-center text-center">  
+    <h2><strong>About the System</strong></h2>
+    <h3><center>The capstone project, entitled “𝘉𝘢𝘳𝘢𝘯𝘨𝘢𝘺 𝘌-𝘴𝘦𝘳𝘷𝘪𝘤𝘦 𝘚𝘺𝘴𝘵𝘦𝘮” is a system purposely designed and developed for barangay use. It will automate the day-to-day operations and transactions in the barangay. The said project will improve the services of the barangay and the overall experience of the residents.</center></h3>
+</div>
+<div class="breadcrumb justify-content-center text-center">        
+<h2><strong>Barangay Emergency Hotline</strong></h2></div>
+<h3><center>contact list</center></h3>
+</body>
+
+
+<script src="../js/alert.js" type="text/javascript"></script>
+<script src="../js/bootstrap.min.js" type="text/javascript"></script>
+
+
+<script src="../js/morris/raphael-2.1.0.min.js" type="text/javascript"></script>
+<script src="../js/morris/morris.js" type="text/javascript"></script>
+<script src="../js/select2.full.js" type="text/javascript"></script>
+
+<script src="../js/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="../js/dataTables.buttons.min.js" type="text/javascript"></script>
+<script src="../js/buttons.print.min.js" type="text/javascript"></script>
+
+<script src="../js/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
+<!-- AdminLTE App -->
+<script src="../js/AdminLTE/app.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+  $(function() {
+      $("#table").dataTable({
+         "aoColumnDefs": [ { "bSortable": false, "aTargets": [ 0,5 ] } ],"aaSorting": []
+      });
+  });
+</script>
+</html>
